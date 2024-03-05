42954cf4c1d4e358d027541bb50b196befb5867e
Alejandro Ajuria <alejoajuria@gmail.com>
2024-03-05 17:24:33 -0600

    inventory keys changed

--- inventory.js
+++ inventory.js
@@ -1,8 +1,8 @@
 class  Inventory {
   constructor(Cantidad, Costo, Sistema_Usuario){
-    this.x = Cantidad;
-    this.y = Costo;
-    this.z = Sistema_Usuario;
+    this.Cantidad = Cantidad;
+    this.Costo = Costo;
+    this.Sistema_Usuario = Sistema_Usuario;
   }
 }
 
