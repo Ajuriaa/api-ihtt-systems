621c290b903371213487d6bf9643013c813b3788
Alejandro Ajuria <alejoajuria@gmail.com>
2024-05-07 09:20:07 -0600

    secure server added

--- api.ts
+++ api.ts
@@ -2,15 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { router as vehicleSystemRouter } from './vehicle-system';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended:  true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/vehicle-system', vehicleSystemRouter);
+app.use('/api', vehicleSystemRouter);
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('El api de gerencia está corriendo en el puerto ' + 3000);
+const httpsOptions = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH || ''),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH || '')
+};
+
+const port = 3000;
+
+https.createServer(httpsOptions, app).listen(port, () => {
+  console.log(`El api de gerencia está corriendo en el puerto ${port}`);
 });
