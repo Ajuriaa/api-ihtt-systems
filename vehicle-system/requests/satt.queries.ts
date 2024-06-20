cd070b7b419aa3fde919208d3428fb8f82ea2838
Alejandro Ajuria <alejoajuria@gmail.com>
2024-06-20 09:20:44 -0600

    Include Documento field in getRequestByBoss and getUserRequestList queries

--- vehicle-system/requests/satt.queries.ts
+++ vehicle-system/requests/satt.queries.ts
@@ -16,7 +16,8 @@ export async function getRequestByBoss(id: string): Promise<any> {
         TB_Solicitudes.Hora_Salida,
         TB_Solicitudes.Hora_Regreso,
         tes.Estado as Estado,
-        tes.ID_Estado_Solicitud as ID_Estado
+        tes.ID_Estado_Solicitud as ID_Estado,
+        TB_Solicitudes.Documento_URL as Documento
       FROM TB_Solicitudes
       JOIN IHTT_RRHH.dbo.v_listado_empleados ON TB_Solicitudes.ID_Empleado = IHTT_RRHH.dbo.v_listado_empleados.ID_Empleado
       JOIN TB_Ciudades ON TB_Solicitudes.ID_Ciudad = TB_Ciudades.ID_Ciudad
@@ -46,7 +47,8 @@ export async function getUserRequestList(id: string): Promise<any> {
         TB_Solicitudes.Hora_Salida,
         TB_Solicitudes.Hora_Regreso,
         tes.Estado as Estado,
-        tes.ID_Estado_Solicitud as ID_Estado
+        tes.ID_Estado_Solicitud as ID_Estado,
+        TB_Solicitudes.Documento_URL as Documento
       FROM TB_Solicitudes
       JOIN IHTT_RRHH.dbo.v_listado_empleados ON TB_Solicitudes.ID_Empleado = IHTT_RRHH.dbo.v_listado_empleados.ID_Empleado
       JOIN TB_Ciudades ON TB_Solicitudes.ID_Ciudad = TB_Ciudades.ID_Ciudad
