662e6356ce4007d6b6228fd48b736234b6a9a94b
Alejandro Ajuria <alejoajuria@gmail.com>
2024-10-28 11:36:29 -0600

    refactor: Update PDFHelper to use system date for request date in requisition formatting

--- services/pdf-generator.service.ts
+++ services/pdf-generator.service.ts
@@ -117,7 +117,7 @@ export class PDFHelper {
     const id = requisition.id;
     const columns = ['No', 'Unidad de medida', 'Producto', 'Cantidad Solicitada','Cantidad Entregada'];
     const formattedSuppliers = this.formatRequisitionsForPDF(requisition);
-    const requestDate = this.getDate(requisition.date);
+    const requestDate = this.getDate(requisition.systemDate);
     const deliverDate = requisition.outputs.length > 0 ? this.getDate(requisition.outputs[0].date) : 'NO ENTREGADO';
     return this.generatePDF(formattedSuppliers, columns, 'Requisición de Materiales Sección Proveeduría', requestDate, deliverDate, department, id);
   }
